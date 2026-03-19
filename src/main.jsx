8c8c935335ffa8cb55b44a751214add342e8c023
bpichhi-star <bpichhi@gmail.com>
2026-03-18 18:13:11 -0700

    Add src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import STBStore from './STBStore.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <STBStore />
+  </React.StrictMode>
+)
